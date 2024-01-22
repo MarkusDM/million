3cb14a4e96ab560c6e39d9ad37c5a2ee7c07b27d
Dmitry <mazarini200@yandex.ru>
2024-01-22 19:16:40 +0500

    first

--- src/js/components/main-s.js
+++ src/js/components/main-s.js
@@ -15,58 +15,58 @@ export const rem = function (rem) {
   }
 };
 
-const mainSwiperOne = new Swiper('.main__swiper-one', {
-  modules: [Navigation, Pagination, EffectFade, Autoplay],
-  slidesPerView: 1,
-  wrapperClass: 'main__swiper-wrapper-one',
-  slideClass: 'main__one-s',
-  speed: 500,
-  loop: true,
-  autoplay: true,
-  effect: 'fade',
-  fadeEffect: {
-    crossFade: true,
-  },
+// const mainSwiperOne = new Swiper('.main__swiper-one', {
+//   modules: [Navigation, Pagination, EffectFade, Autoplay],
+//   slidesPerView: 1,
+//   wrapperClass: 'main__swiper-wrapper-one',
+//   slideClass: 'main__one-s',
+//   speed: 500,
+//   loop: true,
+//   autoplay: true,
+//   effect: 'fade',
+//   fadeEffect: {
+//     crossFade: true,
+//   },
 
-  on: {
-    slideChange: function (mainSwiperOne) {
-      $('.swiper-pagination-bullet-one').removeClass(
-        'swiper-pagination-bullet-one-active'
-      );
-      $(
-        '.swiper-pagination-bullet-one:nth-child(' +
-          ((mainSwiperOne.realIndex % 3) + 1) +
-          ')'
-      ).addClass('swiper-pagination-bullet-one-active');
-    },
-  },
-});
+//   on: {
+//     slideChange: function (mainSwiperOne) {
+//       $('.swiper-pagination-bullet-one').removeClass(
+//         'swiper-pagination-bullet-one-active'
+//       );
+//       $(
+//         '.swiper-pagination-bullet-one:nth-child(' +
+//           ((mainSwiperOne.realIndex % 3) + 1) +
+//           ')'
+//       ).addClass('swiper-pagination-bullet-one-active');
+//     },
+//   },
+// });
 
-const mainSwiperTwo = new Swiper('.main__swiper-two', {
-  modules: [Navigation, Pagination, EffectFade, Autoplay],
-  slidesPerView: 1,
-  wrapperClass: 'main__swiper-wrapper-two',
-  slideClass: 'main__slide-t',
-  speed: 500,
-  loop: true,
-  autoplay: true,
-  effect: 'fade',
-  fadeEffect: {
-    crossFade: true,
-  },
-  on: {
-    slideChange: function (mainSwiperTwo) {
-      $('.swiper-pagination-bullet').removeClass(
-        'swiper-pagination-bullet-active'
-      );
-      $(
-        '.swiper-pagination-bullet:nth-child(' +
-          ((mainSwiperTwo.realIndex % 3) + 1) +
-          ')'
-      ).addClass('swiper-pagination-bullet-active');
-    },
-  },
-});
+// const mainSwiperTwo = new Swiper('.main__swiper-two', {
+//   modules: [Navigation, Pagination, EffectFade, Autoplay],
+//   slidesPerView: 1,
+//   wrapperClass: 'main__swiper-wrapper-two',
+//   slideClass: 'main__slide-t',
+//   speed: 500,
+//   loop: true,
+//   autoplay: true,
+//   effect: 'fade',
+//   fadeEffect: {
+//     crossFade: true,
+//   },
+//   on: {
+//     slideChange: function (mainSwiperTwo) {
+//       $('.swiper-pagination-bullet').removeClass(
+//         'swiper-pagination-bullet-active'
+//       );
+//       $(
+//         '.swiper-pagination-bullet:nth-child(' +
+//           ((mainSwiperTwo.realIndex % 3) + 1) +
+//           ')'
+//       ).addClass('swiper-pagination-bullet-active');
+//     },
+//   },
+// });
 
 const catalogSwiper = new Swiper('.catalog__swiper', {
   modules: [Navigation, Pagination, EffectFade],
